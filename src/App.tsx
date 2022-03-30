55ee0160f25d685bc40537d57dc5286f5b5169a7
Phil <phil.nice.23@gmail.com>
2022-03-30 22:57:39 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import styles from './App.module.css';
 
 const App = () => {
 	return (
-		<div className={styles.App}>
-			<aside className={styles.Aside}>
-				<div className={styles.Aside__logo}>
-					<img src="./img/logo.svg" alt="" className="Aside__logo-img" />
-				</div>
-				<nav className={styles.Aside__nav}></nav>
-			</aside>
-			<main className={styles.Main}>Тест</main>
-		</div>
+		<Router>
+			<div className={styles.App}>
+				<aside className={styles.Aside}>
+					<div className={styles.Aside__logo}>
+						<img src="./img/logo.svg" alt="" className="Aside__logo-img" />
+					</div>
+					<nav className={styles.Aside__nav}>
+						<Link to="/" className={styles.Aside__link}>
+							Итоги
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Заказы
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Сообщения
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Звонки
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Контрагенты
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Документы
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Исполнители
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Отчеты
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							База знаний
+						</Link>
+						<Link to="/" className={styles.Aside__link}>
+							Настройки
+						</Link>
+					</nav>
+				</aside>
+				<main className={styles.Main}>Тест</main>
+			</div>
+		</Router>
 	);
 };
 
