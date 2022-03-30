dcd464d284ab21b561f2d1811b3add6a219a4fb0
Phil <phil.nice.23@gmail.com>
2022-03-30 22:43:17 +0300

    add img and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import styles from './App.module.css';
 
 const App = () => {
-	return <div className={styles.App}></div>;
+	return (
+		<div className={styles.App}>
+			<aside className={styles.Aside}>
+				<div className={styles.Aside__logo}>
+					<img src="./img/logo.svg" alt="" className="Aside__logo-img" />
+				</div>
+				<nav className={styles.Aside__nav}></nav>
+			</aside>
+			<main className={styles.Main}>Тест</main>
+		</div>
+	);
 };
 
 export default App;
